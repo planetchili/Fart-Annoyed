52654447001c7d426cf489f97fdddc3b51f6e6fc
planetchili <planetchili@users.noreply.github.com>
2017-01-14 11:53:07 +0900

    add useful stuff to the framework

--- Engine/Graphics.h
+++ Engine/Graphics.h
@@ -56,6 +56,8 @@ public:
 		PutPixel( x,y,{ unsigned char( r ),unsigned char( g ),unsigned char( b ) } );
 	}
 	void PutPixel( int x,int y,Color c );
+	void DrawRect( int x0,int y0,int x1,int y1,Color c );
+	void DrawCircle( int x,int y,int radius,Color c );
 	~Graphics();
 private:
 	Microsoft::WRL::ComPtr<IDXGISwapChain>				pSwapChain;
