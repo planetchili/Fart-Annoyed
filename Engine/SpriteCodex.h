f07e78ae42778a7e91d89d5f4f65e8ef5936c711
Chili <Chili@CHILI-PC>
2017-02-05 21:00:20 +0900

    add title and end screen sprites

--- Engine/SpriteCodex.h
+++ Engine/SpriteCodex.h
@@ -8,4 +8,8 @@ class SpriteCodex
 public:
 	// centered drawing of 14x14 sprite
 	static void DrawBall( const Vec2& center,Graphics& gfx );
+	// centered drawing of 200x160 sprite
+	static void DrawTitle( const Vec2& center,Graphics& gfx );
+	// centered drawing of 84x64 sprite
+	static void SpriteCodex::DrawGameOver( const Vec2& center,Graphics& gfx );
 };
