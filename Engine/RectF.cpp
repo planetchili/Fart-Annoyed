13c7c1a33d981c69c1652de2b559735ac0a816ee
Chili <Chili@CHILI-PC>
2017-02-05 17:59:02 +0900

    add containment test to rectf

--- Engine/RectF.cpp
+++ Engine/RectF.cpp
@@ -27,6 +27,12 @@ bool RectF::IsOverlappingWith( const RectF& other ) const
 		&& bottom > other.top && top < other.bottom;
 }
 
+bool RectF::IsContainedBy( const RectF & other ) const
+{
+	return left >= other.left && right <= other.right &&
+		top >= other.top && bottom <= other.bottom;
+}
+
 RectF RectF::FromCenter( const Vec2 & center,float halfWidth,float halfHeight )
 {
 	const Vec2 half( halfWidth,halfHeight );
--- Engine/RectF.h
+++ Engine/RectF.h
@@ -10,6 +10,7 @@ public:
 	RectF( const Vec2& topLeft,const Vec2& bottomRight );
 	RectF( const Vec2& topLeft,float width,float height );
 	bool IsOverlappingWith( const RectF& other ) const;
+	bool IsContainedBy( const RectF& other ) const;
 	static RectF FromCenter( const Vec2& center,float halfWidth,float halfHeight );
 	RectF GetExpanded( float offset ) const;
 	Vec2 GetCenter() const;
